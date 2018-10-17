d949d15b1519794b6f41443358a8baa51f6b609d
Mary Coe <marykcoe@btinternet.com>
2018-10-17 16:58:27 +0100

    Altered to reweight correctly by forming a histogram of the weights as opposed to using formula in Am J Phys Wilding article. Added function which calculates the mean of the distribution and calculates the areas under the phases respctively.

--- histogram_rewighting.py
+++ histogram_rewighting.py
@@ -15,6 +15,7 @@ import sys
 sys.path.append(DL_MONTE_HOME)
 import htk.util
 import htk.histogram"""
+
 colours = ['plum', 'lightblue', 'pink', 'lightgreen', 'khaki', 'tomato', 'silver', 'turquoise', 'orange']
 marker_colours = ['purple','blue', 'violet', 'darkgreen', 'gold', 'crimson', 'grey','teal', 'coral']
 
@@ -65,33 +66,89 @@ def reweight_mu(num_sims, file_path, volume, mus, mu_org, run):
 
     if(len(mus)>8):
         print("The maximum number of mus this program can reweight to is 8. Cut off will occur after the 8th my.")
+     
+    max_mols = 0
     
-    max_mols, mols_hist = find_prob_dist(num_sims,file_path,volume,run)
-    shape_y, shape_x= mols_hist.shape
+    #Find maximum number of molecules across sims
+    for sim in range(num_sims):
+        input_data = np.genfromtxt(file_path + 'run_' + str(run) + '_'  + str(sim) + '/data.dat', usecols=(2))
+        if(max_mols<max(input_data)):
+            max_mols = max(input_data)
+    
+    mols_hist = np.zeros((int(max_mols)+1,len(mus)+2))
+    total = np.zeros(len(mus)+1)
+    for entry in range(int(max_mols)+1):
+        mols_hist[entry,0] = (entry*180.15)/(volume*6.022)
 
-    new_cols = np.zeros((shape_y,len(mus)))
-    mols_hist = np.append(mols_hist, new_cols, axis=1)
+    for sim in range(num_sims):
+        input_data = np.genfromtxt(file_path + 'run_' + str(run) + '_'  + str(sim) + '/data.dat', usecols=(2))
+        
+        for entry in input_data:
+            mols_hist[int(entry),1] +=1
+        
+        total[0] = np.sum(mols_hist[:,1]*((180.15)/(volume*6.022)))
+        
+        #Generate new distributions
+        for indx,mu in enumerate(mus):
+            print("mu is {} indx is {}\n".format(mu,indx))
+            dmu = mu_org - mu
+            for entry in input_data:
+                mols_hist[int(entry),indx+2] += np.exp(-1.0*dmu*entry)
+            total[indx+1] = np.sum(((180.15)/(volume*6.022))*mols_hist[:,indx+2])
     
-    #Generate new distributions
-    for indx,mu in enumerate(mus):
-        print("mu is {} indx is {}\n".format(mu,indx))
-        dmu = mu_org - mu
-        for entry in range(int(max_mols+1)):
-            mols_hist[entry,indx+2] = np.exp(-1*dmu*entry)*mols_hist[entry,1]
     
-    #Finally, plot the output on one graph and save to file.
+    for col in range(len(mus)+1):
+        mols_hist[:,col+1]/=total[col]
+        
+    with open(file_path + "mols_profile", 'w') as dprof:
+        for entry in range(0,int(max_mols)+1):
+            dprof.write("{:.5f} {:.5f} ".format(mols_hist[entry,0],mols_hist[entry,1]))
+            for mu in range(len(mus)):
+                dprof.write("{:.5f} ".format(mols_hist[entry,mu+2]))
+            dprof.write("\n")
+		
+
+        
+   #Finally, plot the output on one graph and save to file.
     plt.figure()
-    plt.plot(mols_hist[:,0],mols_hist[:,1], color=colours[0],marker = '^', markersize=2, markerfacecolor=marker_colours[0], markeredgecolor=marker_colours[0], label = r'$original\ \mu=' + str(mu_org) + '$')
+    plt.plot(mols_hist[:,0],mols_hist[:,1], color=colours[0], marker = '^', markersize=2, markerfacecolor=marker_colours[0], markeredgecolor=marker_colours[0],  label = r'$\mu=' + str(mu_org) + '$')
     for indx,mu in enumerate(mus):
-        plt.plot(mols_hist[:,0], mols_hist[:,indx+2], color = colours[indx+1],marker = 'o', markersize=2, markerfacecolor=marker_colours[indx+1], markeredgecolor=marker_colours[indx+1], label = r'$\mu=' + str(mu) + '$')
+       plt.plot(mols_hist[:,0], mols_hist[:,indx+2], color = colours[indx+1],marker = 'o', markersize=2, markerfacecolor=marker_colours[indx+1], markeredgecolor=marker_colours[indx+1], label = r'$\mu=' + str(mu) + '$')
 
     plt.legend()
     plt.xlabel(r'$\rho (g/cm^3)$')
-    plt.ylabel('Probability')
+    plt.ylabel('Probability Density Function')
     plt.xlim(0, (max_mols*180.15)/(volume*6.022))
-    plt.ylim(0, max(map(max,mols_hist[:,1:num_sims+2])))
-    plt.savefig(file_path + '/hist_reweighting_mu.pdf')
+    plt.ylim(0, 1.1*max(map(max,mols_hist[:,1:(len(mus)+2)])))
+    plt.savefig(file_path + '/hist_reweighting_mu_' + str(run) + '.pdf')
+    calc_areas(mols_hist,mus,mu_org, volume)
 
+
+def calc_areas(mols_hist, mus, mu_org, volume):
+    
+    comparisons = np.zeros((len(mus)+1,5))
+    
+    comparisons[0,0] = mu_org
+    comparisons[0,1] = np.sum(mols_hist[:,1]*mols_hist[:,0])
+    break_point = np.argmin(mols_hist[:,1].cumsum() < comparisons[0,1])
+    comparisons[0,1] = mols_hist[break_point,0]
+    comparisons[0,2] = np.sum(((180.15)/(volume*6.022)*mols_hist[0:break_point,1]))
+    comparisons[0,3] = np.sum(((180.15)/(volume*6.022)*mols_hist[break_point:len(mols_hist),1]))
+    comparisons[0,4] = comparisons[0,3]-comparisons[0,2]
+    
+    for indx,mu in enumerate(mus):
+        comparisons[indx+1,0] = mu
+        comparisons[indx+1,1] = np.sum(mols_hist[:,indx+2]*mols_hist[:,0])
+        break_point = np.argmin(mols_hist[:,indx+2].cumsum() < comparisons[indx+1,1])
+        comparisons[indx+1,1] = mols_hist[break_point,0]
+        comparisons[indx+1,2] = np.sum(((180.15)/(volume*6.022)*mols_hist[0:break_point,indx+2]))
+        comparisons[indx+1,3] = np.sum(((180.15)/(volume*6.022)*mols_hist[break_point:len(mols_hist),indx+2]))
+        comparisons[indx+1,4] = comparisons[indx+1,3]-comparisons[indx+1,2]
+    
+    for line in range(len(mus)+1):
+        print("{:.2f} {:.5f} {:.5f} {:.5f} {:.5f}".format(comparisons[line,0], comparisons[line,1], comparisons[line,2], comparisons[line,3], comparisons[line,4]))
+   
+        
 def reweight_beta(num_sims, file_path, volume, mus, mu_org, run):
     
     """ Reweights the temperature of the system. Because in the simulation the parameters are defined as
@@ -123,4 +180,4 @@ def reweight_beta(num_sims, file_path, volume, mus, mu_org, run):
     plt.ylim(0, max(map(max,mols_hist[:,1:num_sims+2])))
     plt.savefig(file_path + '/hist_reweighting.pdf')
             
-reweight_mu(4, '../864.5K_multiprocessing/mu_5.37/', pow(4*1.8*2.3925,3), [-5.35,-5.355, -5.36, -5.365, -5.375, -5.38, -5.385], -5.37, 0)
+reweight_mu(4, '../Results/multicanonical/864.5K/mu_5.37/', pow(4*1.8*2.3925,3), [-5.3,-5.32,-5.34,-5.36], -5.37, 0)
